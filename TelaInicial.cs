4b2294e6da39b77db3e6b9571fdb0f852ddd512a
danie <daniel.jr2501@gmail.com>
2023-12-27 15:19:56 -0300

    Organização dos arquivos e pastas do projeto

--- TelaInicial.cs
+++ TelaInicial.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SistemaGestaoEmpresarial
 {
-    public partial class Form1 : Form
+    public partial class TelaInicial : Form
     {
-        public Form1()
+        public TelaInicial()
         {
             InitializeComponent();
         }
